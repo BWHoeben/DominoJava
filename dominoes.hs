be47bce2702063ed4ead2ff43f9787655d9ac6c5
BWHoeben <bart.hoeben@nedap.com>
2018-09-17 10:10:40 +0200

    Update haskell

--- dominoes.hs
+++ dominoes.hs
@@ -1,3 +1,6 @@
+import System.IO
+
+type Board = ([Int], [Int], [Int], [Int], [Int], [Int], [Int])
 
 helloWorld :: String
 helloWorld = "Hello World"
@@ -12,5 +15,20 @@ getInput = ([5, 0, 3, 5, 4, 5, 5],
             [4, 1, 2, 0, 4, 6, 3],
             [6, 1, 0, 6, 1, 5, 1])
 
-printBoard :: ([Int], [Int], [Int], [Int], [Int], [Int], [Int], [Int]) -> String
-printBoard xss 
+getInput2 :: Board
+getInput2 = ([5, 4, 3, 6, 5, 3, 4, 6],
+             [0, 6, 0, 1, 2, 3, 1, 1],
+             [3, 2, 6, 5, 0, 4, 2, 0],
+             [5, 3, 6, 2, 3, 2, 0, 6],
+             [4, 0, 4, 1, 0, 0, 4, 1],
+             [5, 2, 2, 4, 4, 1, 6, 5],
+             [5, 5, 3, 6, 1, 2, 3, 1])
+
+printList :: [Int] -> String
+printList [] = ""
+printList xs = e ++ " " ++ printList (tail xs)
+               where e = if (head xs) < 0 then "X" else show (head xs)
+
+
+printBoard :: Board -> IO ()
+printBoard (a, b, c , d, e, f, g) = putStr(printList a ++ "\n" ++ printList b ++ "\n" ++ printList c ++ "\n" ++ printList d ++ "\n" ++ printList e ++ "\n" ++ printList f ++ "\n" ++ printList g ++ "\n")
